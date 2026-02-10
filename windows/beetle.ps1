7df0f49b0fe0c135ab9e374fb25a0c2cab1d2c9e
somanshu5011 <somanshumahajan@gmail.com>
2026-02-10 21:47:09 +0530

    great start is the best start

---
+++ windows/beetle.ps1
@@ -0,0 +1,38 @@
+$beetle = @"
+          ##########        ##########
+          ##      ##        ##      ##
+          ####    ####    ####    ####      
+                    ########                
+                    ########                
+            ########################        
+      ##    ########################       
+      ####################################   BBBBBBBB  EEEEEEEE  EEEEEEEE  TTTTTTTT  LL        EEEEEEEE
+            ########################    ##   BB    BB  EE        EE           TT     LL        EE
+      ##    ########################         BBBBBBBB  EEEEEE    EEEEEE       TT     LL        EEEEEE
+      ####################################   BB    BB  EE        EE           TT     LL        EE
+            ########################    ##   BBBBBBBB  EEEEEEEE  EEEEEEEE     TT     LLLLLLL   EEEEEEEE
+      ##    ########################
+      ####################################
+            ########################    ##
+            ########################
+                ################
+                  ##        ##       
+                  ##        ##  
+
+        +----------------------------+
+        |           BEETLE           |
+        +----------------------------+
+"@
+
+
+foreach ($line in $beetle -split "`n") {
+    foreach ($char in $line.ToCharArray()) {
+        if ($char -eq '#') {
+            Write-Host $char -NoNewline -ForegroundColor DarkGreen
+        }
+        else {
+            Write-Host $char -NoNewline -ForegroundColor Blue
+        }
+    }
+    Write-Host ""
+}
