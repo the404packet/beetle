48c854b51611925fcf3da69d0e8aca4ddf071e2c
somanshu5011 <somanshumahajan@gmail.com>
2026-04-13 15:34:12 +0530

    audit to get form ram

--- ubuntu/beetle_shell/audit/system_maintenance/system_file_permissions/group_file_permissions.sh
+++ ubuntu/beetle_shell/audit/system_maintenance/system_file_permissions/group_file_permissions.sh
@@ -3,16 +3,25 @@
 NAME="/etc/group file permissions"
 SEVERITY="basic"
 
+GREEN="\e[32m"
+RED="\e[31m"
+RESET="\e[0m"
+
 FILE="/etc/group"
 
-# File must exist
+[ -f "$PERM_RAM_STORE" ] && source "$PERM_RAM_STORE"
+
+EXPECTED_MODE=$(get_perm "$FILE" mode)
+EXPECTED_OWNER=$(get_perm "$FILE" owner)
+EXPECTED_GROUP=$(get_perm "$FILE" group)
+
 [ -f "$FILE" ] || exit 2
 
 mode=$(stat -Lc '%a' "$FILE" 2>/dev/null) || exit 2
-uid=$(stat -Lc '%u' "$FILE" 2>/dev/null) || exit 2
-gid=$(stat -Lc '%g' "$FILE" 2>/dev/null) || exit 2
+owner=$(stat -Lc '%U' "$FILE" 2>/dev/null) || exit 2
+group=$(stat -Lc '%G' "$FILE" 2>/dev/null) || exit 2
 
-if [[ "$uid" -eq 0 && "$gid" -eq 0 && "$mode" -le 644 ]]; then
+if [[ "$owner" == "$EXPECTED_OWNER" && "$group" == "$EXPECTED_GROUP" && "$mode" -le "$EXPECTED_MODE" ]]; then
     echo -e "${GREEN}HARDENED${RESET}"
 else
     echo -e "${RED}NOT HARDENED${RESET}"
