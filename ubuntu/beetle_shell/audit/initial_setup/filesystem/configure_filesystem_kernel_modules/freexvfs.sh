b99085ebda2f9b1e46305b2bd2da2f50f92c3ebb
somanshu5011 <somanshumahajan@gmail.com>
2026-04-21 20:19:26 +0530

    filesystem done

---
+++ ubuntu/beetle_shell/audit/initial_setup/filesystem/configure_filesystem_kernel_modules/freexvfs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+NAME="ensure freevxfs kernel module is not available"
+GREEN="\e[32m"; RED="\e[31m"; RESET="\e[0m"
+[ -f "$INITIAL_SETUP_RAM_STORE" ] && source "$INITIAL_SETUP_RAM_STORE" || { echo -e "${RED}NOT HARDENED${RESET}"; exit 0; }
+
+beetle_module_audit "freevxfs" "fs" \
+    && echo -e "${GREEN}HARDENED${RESET}" \
+    || echo -e "${RED}NOT HARDENED${RESET}"
+exit 0
